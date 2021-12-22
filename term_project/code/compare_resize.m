bfa5e850b3e17310137459afdbf65aeacb90d23f
Refstop <bhbhchoi@gmail.com>
2021-12-23 01:34:05 +0900

    save

--- term_project/code/compare_resize.m
+++ term_project/code/compare_resize.m
@@ -1,34 +1,43 @@
 clc;clear all;close all;
-input = imread('[크기변환]baby.png');
-original = imread('baby.png');
+baby_small = imread('[크기변환]baby.png');
+bird_small = imread('[크기변환]bird.png');
+butterfly_small = imread('[크기변환]butterfly.png');
+head_small = imread('[크기변환]head.png');
+woman_small = imread('[크기변환]woman.png');
+baby = imread('baby.png');
+bird = imread('bird.png');
+butterfly = imread('butterfly.png');
+head = imread('head.png');
+woman = imread('woman.png');
+
+compareResize(baby, baby_small, "baby");
+compareResize(bird, bird_small, "bird");
+compareResize(butterfly, butterfly_small, "butterfly");
+compareResize(head, head_small, "head");
+compareResize(woman, woman_small, "woman");
+
+function compareResize(original, resized, filename)
 %% myResize
-nn_output = myResizeNN(input, 4);
-bil_output = myResizeBil(input, 4);
-bic_output = myResizeBic(input, 4);
+nn_output = myResizeNN(resized, 4);
+bil_output = myResizeBil(resized, 4);
+bic_output = myResizeBic(resized, 4);
 
-figure(1);
-subplot(221); imshow(original); title("Original");
-subplot(222); imshow(nn_output); title("myNN");
-subplot(223); imshow(bil_output); title("myBil");
-subplot(224); imshow(bic_output); title("myBic");
-truesize;
-
-fprintf("myNN psnr: %.8f\n", psnr(original, nn_output));
-fprintf("myBil psnr: %.8f\n", psnr(original, bil_output));
-fprintf("myBic psnr: %.8f\n\n", psnr(original, bic_output));
+fig1 = figure;
+subplot(221); imshow(original); title("Original", 'FontSize', 20);
+subplot(222); imshow(nn_output); title("myNN, " + string(psnr(original, nn_output)), 'FontSize', 20);
+subplot(223); imshow(bil_output); title("myBil, " + string(psnr(original, bil_output)), 'FontSize', 20);
+subplot(224); imshow(bic_output); title("myBic, " + string(psnr(original, bic_output)), 'FontSize', 20);
+saveas(fig1, filename+"_myResize.png");
 
 %% bulitin
-nn_output_b = imresize(input, 4, 'nearest');
-bil_output_b = imresize(input, 4, 'bilinear');
-bic_output_b = imresize(input, 4, 'bicubic');
+nn_output_b = imresize(resized, 4, 'nearest');
+bil_output_b = imresize(resized, 4, 'bilinear');
+bic_output_b = imresize(resized, 4, 'bicubic');
 
-figure(2);
-subplot(221); imshow(original); title("Original");
-subplot(222); imshow(nn_output_b); title("NN");
-subplot(223); imshow(bil_output_b); title("Bil");
-subplot(224); imshow(bic_output_b); title("Bic");
-truesize;
-
-fprintf("NN psnr: %.8f\n", psnr(original, nn_output_b));
-fprintf("Bil psnr: %.8f\n", psnr(original, bil_output_b));
-fprintf("Bic psnr: %.8f\n", psnr(original, bic_output_b));
+fig2 = figure;
+subplot(221); imshow(original); title("Original", 'FontSize', 20);
+subplot(222); imshow(nn_output_b); title("NN, " + string(psnr(original, nn_output_b)), 'FontSize', 20);
+subplot(223); imshow(bil_output_b); title("Bil, " + string(psnr(original, bil_output_b)), 'FontSize', 20);
+subplot(224); imshow(bic_output_b); title("Bic, " + string(psnr(original, bic_output_b)), 'FontSize', 20);
+saveas(fig2, filename+"_builtin.png");
+end
